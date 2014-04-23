b8091028148a9841674a74625509b34ee8140dd5
Nathan Francy <nathanfrancy@gmail.com>
2014-04-23 09:12:03 -0500

    cleanup
    * adding first level of documentation
    * changed a couple function names to begin with ‘tvm’ to be consistent
    * removed a duplicate function
    *

--- js/tvm.js
+++ js/tvm.js
@@ -1,47 +1,107 @@
-// regular
-
+/**
+ * Returns present value of an investment
+ * @param {Decimal} fv : future value
+ * @param {Decimal} n : time
+ * @param {Decimal} r : interest rate
+ * @returns {Decimal} pv : present value
+ */
 function tvmPV(fv, n, r) {
     return fv * Math.pow((1 + r), (-(n)));
 }
 
-function tvmFV(pv, ir, n) {
-    return pv * Math.pow(1+ir, n);
+/**
+ * Returns the future value of an investment given pv, r, n
+ * @param {Decimal} pv : present value
+ * @param {Decimal} r : interest rate
+ * @param {Decimal} n : time
+ * @returns {Decimal} fv : future value
+ */
+function tvmFV(pv, r, n) {
+    return pv * Math.pow(1 + r, n);
 }
 
-function tvmAIR(pv, fv, n) {
+/**
+ * Returns an estimated annual interest rate with pv, fv, n
+ * @param {Decimal} pv : present value
+ * @param {Decimal} fv : future value
+ * @param {Decimal} n : time
+ * @returns {Decimal} r : annual interest rate
+ */
+function tvmR(pv, fv, n) {
     return Math.pow((fv / pv), (1 / n)) - 1;
 }
 
-function doubleMoney(r) {
+/**
+ * Returns the time (n) it will take to double your money given an annual interest rate
+ * @param {Decimal} r : annual interest rate
+ * @returns {Decimal} n : time
+ */
+function tvmDoubleMoney(r) {
     return Math.log(2) / Math.log(1 + r);
 }
 
-function tvmIR(pv, fv, n) {
-    return Math.pow((fv / pv), (1 / 10)) - 1;
+/**
+ * Returns the time of the investment given pv, fv and annual interest rate
+ * @param {Decimal} pv : present value
+ * @param {Decimal} fv : future value
+ * @param {Decimal} r : annual interest rate
+ * @returns {Decimal} n : time
+ */
+function tvmN(pv, fv, r) {
+    return Math.log(fv / pv) / Math.log(r + 1);
 }
 
-function tvmN(pv, fv, ir) {
-    return Math.log(fv / pv) / Math.log(ir + 1);
-}
-
-// annuity
-
-function tvmPVad(r, n, pmt) {
-	return pmt * ( (1 - ( Math.pow((1 + r), (-(n))) )) / r ) * (1 + r);
-}
-
+/**
+ * Returns the present value of an ordinary annuity given r, n, pmt
+ * @param {Decimal} r : annual interest rate
+ * @param {Decimal} n : time
+ * @param {Decimal} pmt : payment
+ * @returns {Decimal} pv : present value of an ordinary annuity
+ */
 function tvmPVoa(r, n, pmt) {
     return pmt * ((1 - (Math.pow( (1 + r), (-(n))))) / r);
 }
 
+/**
+ * Returns the future value of an ordinary annuity given r, n, pmt
+ * @param {Decimal} r : annual interest rate
+ * @param {Decimal} n : time
+ * @param {Decimal} pmt : payment
+ * @returns {Decimal} fv : future value of an ordinary annuity
+ */
 function tvmFVoa(r, n, pmt) {
     return pmt * (((Math.pow((1 + r),n)) - 1) / r);
 }
 
+/**
+ * Returns the present value of an annuity due given an interest rate, time and payment
+ * @param {Decimal} r : annual interest rate
+ * @param {Decimal} n : time
+ * @param {Decimal} pmt : payment
+ * @returns {Decimal} pv : present value of an annuity due
+ */
+function tvmPVad(r, n, pmt) {
+	return pmt * ( (1 - ( Math.pow((1 + r), (-(n))) )) / r ) * (1 + r);
+}
+
+/**
+ * Returns the future value of an annuity due given r, n, pmt
+ * @param {Decimal} r : annual interest rate
+ * @param {Decimal} n : time
+ * @param {Decimal} pmt : payment
+ * @returns {Decimal} fv : future value of an annuity due
+ */
 function tvmFVad(r, n, pmt) {
     return pmt * (( Math.pow((1 + r), n) - 1) / r ) * (1 + r);
 }
 
+/**
+ * Returns the payment of an ordinary annuity given n, r, pv
+ * @param {Decimal} n : time
+ * @param {Decimal} r : annual interest rate
+ * @param {Decimal} pv : present value of an annuity
+ * @returns {Decimal} pmt : payment needed in an ordinary annuity
+ */
 function tvmPMToa(n, r, pv) {
 	return pv / (((1 - Math.pow((1 + r), (-(n))))) / r );
 }
