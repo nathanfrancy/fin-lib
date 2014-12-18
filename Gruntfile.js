a6a5c8cec381401800e87559c2014230604a816b
Nathan Francy <nathanfrancy@gmail.com>
2014-12-17 20:17:16 -0600

    get rid of the unnecessary stuff
    like demos that were worthless, css that didn’t really pertain to the
    project. just the tvm script and grunt to compile it
    one test file to test functions in the console :)

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,7 @@ module.exports = function(grunt) {
     uglify: {
       my_target: {
         files: {
-          'build/js/tvm.min.js': 'js/tvm.js',
-          'build/js/scripts.min.js' : 'js/scripts.js'
+          'build/js/tvm.min.js': 'js/tvm.js'
         }
       }
     },
@@ -15,38 +14,14 @@ module.exports = function(grunt) {
     uglify: {
       build: {
         src: 'js/tvm.js',
-        dest: 'build/js/tvm.min.js'
-      },
-      buil : {
-        src: 'js/scripts.js',
-        dest: 'build/js/scripts.min.js'
+        dest: 'dist/tvm.min.js'
       }
-    },
-
-    sass : {
-      dist : {
-        options: { 
-          style: 'compressed'
-        },
-        files : { 
-          'build/css/style.min.css' : 'css/style.scss',
-          'build/css/bootstrap.min.css' : 'css/bootstrap.scss'
-        }
-      }
-    },
-
-    watch : {
-      files: ['css/style.scss', 'css/bootstrap.scss', 'css/bootstrap/*.scss'],
-      tasks: 'sass'
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('min', ['uglify', 'sass']);
   grunt.registerTask('default', ['uglify']);
 
 };
--- index.php
+++
@@ -1,2 +0,0 @@
-<?php
-header("Location: templates/index.php");
